54eb551378ccb79bdf8b8e2f89fc8539df81ed6d
Lucas Pope <lpopedv@proton.me>
2024-10-14 18:33:40 -0300

    feat: add tailwind css lsp

--- doom/.config/doom/packages.el
+++ doom/.config/doom/packages.el
@@ -1,2 +1,3 @@
 (package! protobuf-mode)
 (package! prisma-mode :recipe (:host github :repo "pimeys/emacs-prisma-mode" :branch "main"))
+(package! lsp-tailwindcss :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
