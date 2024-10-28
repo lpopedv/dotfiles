28e3f93949f89345ba0cb6950201c76eceac9f5c
Lucas Pope <lpopedv@proton.me>
2024-10-28 11:29:10 -0300

    feat: more adjusts in lazyvim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,9 +6,19 @@ vim.keymap.set("i", "<C-h>", "<Left>", { silent = true })
 vim.keymap.set("i", "<C-j>", "<Down>", { silent = true })
 vim.keymap.set("i", "<C-k>", "<Up>", { silent = true })
 vim.keymap.set("i", "<C-l>", "<Right>", { silent = true })
+vim.keymap.set("i", "<C-BS>", "<C-w>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>b[", ":bprevious<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>b]", ":bnext<CR>", { noremap = true, silent = true })
 
 -- set conceal
 vim.cmd([[
   set conceallevel=0 
   set concealcursor=""
 ]])
+
+-- Telescope custom options
+require("telescope").setup({
+  defaults = {
+    file_ignore_patterns = { "node_modules" },
+  },
+})
