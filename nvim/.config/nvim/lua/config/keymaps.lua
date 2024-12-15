7c7ae1eec71b4ae6231f64b9ef0a65fce074aa6b
Lucas Pope <lpopedv@proton.me>
2024-12-15 19:23:23 -0300

    feat: adjusts and updates

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Save file
+vim.keymap.set("n", "<leader>bs", ":w<CR>")
+
+-- Insert mode keybindings
+vim.keymap.set("i", "<C-h>", "<Left>", { silent = true })
+vim.keymap.set("i", "<C-j>", "<Down>", { silent = true })
+vim.keymap.set("i", "<C-k>", "<Up>", { silent = true })
+vim.keymap.set("i", "<C-l>", "<Right>", { silent = true })
+vim.keymap.set("i", "<C-BS>", "<C-w>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>b[", ":bprevious<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>b]", ":bnext<CR>", { noremap = true, silent = true })
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,15 +1,3 @@
--- Save file
-vim.keymap.set("n", "<leader>bs", ":w<CR>")
-
--- Insert mode keybindings
-vim.keymap.set("i", "<C-h>", "<Left>", { silent = true })
-vim.keymap.set("i", "<C-j>", "<Down>", { silent = true })
-vim.keymap.set("i", "<C-k>", "<Up>", { silent = true })
-vim.keymap.set("i", "<C-l>", "<Right>", { silent = true })
-vim.keymap.set("i", "<C-BS>", "<C-w>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>b[", ":bprevious<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>b]", ":bnext<CR>", { noremap = true, silent = true })
-
 -- set conceal
 vim.cmd([[
   set conceallevel=0 
