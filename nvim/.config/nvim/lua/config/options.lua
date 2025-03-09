dd1323edad3f76d7e77bbd684422f25e153e9b27
Lucas Pope <lpopedv@proton.me>
2025-03-09 19:20:42 -0300

    feat: some configs

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- scroll options
+vim.opt.scrolloff = 8
+
+-- Backup files, swap files and undo
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
