147bea523d5189e1c0b732bb87a2ce427e3bad8e
Lucas Pope <lpopedv@proton.me>
2025-03-09 13:03:59 -0300

    feat: overrides line numbers color

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -8,6 +8,8 @@ return {
           CursorLine = { bg = "#3c4048" },
           Directory = { fg = "#a5a7ac", bg = "none", bold = true },
           NonText = { fg = "#6c7a8b" },
+          LineNr = { fg = "#a5a7ac" },
+          CursorLineNr = { fg = "#eda160", bold = true },
         },
       })
     end,
