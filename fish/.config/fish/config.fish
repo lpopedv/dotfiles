4255e4467ff860d57dd0b47398830b4b782d1458
Lucas Pope <lpopedv@proton.me>
2025-04-01 21:19:08 -0300

    feat: asdf + doom emacs config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,21 @@ if status is-interactive
     # Aliases 
     alias v nvim
 
+    # ASDF configuration code
+    if test -z $ASDF_DATA_DIR
+        set _asdf_shims "$HOME/.asdf/shims"
+    else
+        set _asdf_shims "$ASDF_DATA_DIR/shims"
+    end
+
+    if not contains $_asdf_shims $PATH
+        set -gx --prepend PATH $_asdf_shims
+    end
+    set --erase _asdf_shims
+
     # Starship Theme
     starship init fish | source
+
+    # Doom emacs
+    set -gx PATH "$HOME/.config/emacs/bin" $PATH
 end
