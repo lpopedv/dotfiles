01231a61745b6ed23f6ba4d0d8d37c47e6d31e4b
Lucas Pope <lpopedv@proton.me>
2025-07-21 21:39:16 -0300

    Improve doom emacs config + terminal support

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -1,93 +1,24 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Lucas Pope"
       user-mail-address "lpopedv@proton.me")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;
 (setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 17)
       doom-variable-pitch-font (font-spec :family "JetBrainsMono Nerd Font" :size 16))
 
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-ayu-mirage)
-
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq doom-theme 'doom-ayu-dark)
 (setq display-line-numbers-type 'relative)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-;; elixir
+;; Elixir
 (setq flycheck-elixir-credo-strict t)
 (setq lsp-elixir-fetch-deps t)
 
-;; lsp configs
+;; LSP
 (setq lsp-enable-file-watchers t)
 (setq lsp-file-watch-threshold 20000)
 
-;; move with vim keys in insert mode
+;; Move with vim keys in insert mode
 (after! evil
   (evil-define-key 'insert 'global
     (kbd "C-h") 'backward-char
@@ -95,10 +26,9 @@
     (kbd "C-k") 'evil-previous-line
     (kbd "C-l") 'forward-char))
 
-;; cursor margin
 (setq scroll-margin 6)
 
-;; lsp fuzzy matching autocomplete
+;; Completion
 (use-package! orderless
   :init
   (setq orderless-matching-styles '(orderless-flex orderless-regexp)
@@ -122,19 +52,36 @@
 (after! lsp-mode
   (setq lsp-completion-provider :capf))
 
-;; transparency
-(set-frame-parameter (selected-frame) 'alpha '(95 . 95))
-(add-to-list 'default-frame-alist '(alpha . (95 . 95)))
+;; GUI only
+(when (display-graphic-p)
+  (set-frame-parameter (selected-frame) 'alpha '(95 . 95))
+  (add-to-list 'default-frame-alist '(alpha . (95 . 95))))
 
-;; Elixir lsp
 (after! lsp-mode
   (setq lsp-elixir-ls-server-file (expand-file-name "~/.lsp/elixir-ls/release/language_server.sh")))
 
-(use-package! elcord
-  :config
-  (setq elcord-editor-icon "emacs_icon")
-  (setq elcord-use-major-mode-as-main-icon t)
-  (elcord-mode))
-
 (setq shell-file-name "/usr/bin/fish")
 (setq explicit-shell-file-name "/usr/bin/fish")
+
+;; Terminal configuration
+(unless (display-graphic-p)
+  (require 'term)
+  (setq xterm-extra-capabilities '(setSelection getSelection))
+  (load-theme 'doom-one t)
+
+  ;; Evil cursor shapes and colors
+  (setq evil-normal-state-cursor    '(box "#81A1C1")
+        evil-insert-state-cursor    '(bar "#A3BE8C")
+        evil-visual-state-cursor    '(hollow "#D08770")
+        evil-replace-state-cursor   '(hbar "#BF616A")
+        evil-operator-state-cursor  '(hollow "#81A1C1")
+        evil-motion-state-cursor    '(box "#81A1C1")
+        evil-emacs-state-cursor     '(bar "#B48EAD"))
+
+  (when (fboundp 'evil-refresh-cursor)
+    (add-hook 'post-command-hook #'evil-refresh-cursor))
+
+  ;; Clipboard support
+  (when (getenv "DISPLAY")
+    (setq select-enable-clipboard t
+          select-enable-primary t)))
