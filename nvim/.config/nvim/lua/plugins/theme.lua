8d06b1d5c0c335c07aee503f2e7be61fc7d7665f
Lucas Pope <lpopedv@proton.me>
2025-08-09 00:19:31 -0300

    Set tokyonight theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,9 +1,60 @@
 return {
 	{ "ellisonleao/gruvbox.nvim" },
+	{ "folke/tokyonight.nvim" },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			flavour = "mocha",
+			background = {
+				light = "latte",
+				dark = "mocha",
+			},
+			transparent_background = false,
+			show_end_of_buffer = false,
+			term_colors = false,
+			dim_inactive = {
+				enabled = true,
+				shade = "dark",
+				percentage = 0.15,
+			},
+			no_italic = false,
+			no_bold = false,
+			no_underline = false,
+			styles = {
+				comments = { "italic" },
+				conditionals = { "italic" },
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+				operators = {},
+			},
+			color_overrides = {},
+			custom_highlights = {},
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				notify = false,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+			},
+		},
+	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "gruvbox",
+			colorscheme = "catppuccin",
 		},
 	},
 }
