2995611f97012d2e847c3b2fe0272c6560f52adf
Lucas Pope <lpopedv@proton.me>
2026-05-09 21:27:51 -0300

    Install Oil

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,8 +5,10 @@ vim.pack.add({
   'https://github.com/ibhagwan/fzf-lua',
   'https://github.com/nvim-treesitter/nvim-treesitter',
   'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/stevearc/oil.nvim',
 })
 
 require('plugins.fzf')
 require('plugins.treesitter')
 require('plugins.lsp')
+require('plugins.oil')
