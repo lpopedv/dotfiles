3d8e71d096cd6c66eaf462eab8b66650fa1657b7
Lucas Pope <lpopedv@proton.me>
2026-05-09 21:28:46 -0300

    Add lazygit keymap

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -33,6 +33,26 @@ vim.keymap.set('i', '<C-j>', '<Down>',  { desc = 'Move cursor down' })
 vim.keymap.set('i', '<C-k>', '<Up>',    { desc = 'Move cursor up' })
 vim.keymap.set('i', '<C-l>', '<Right>', { desc = 'Move cursor right' })
 
+-- Lazygit
+vim.keymap.set('n', '<leader>gg', function()
+  local buf = vim.api.nvim_create_buf(false, true)
+  local width  = math.floor(vim.o.columns * 0.9)
+  local height = math.floor(vim.o.lines * 0.9)
+  vim.api.nvim_open_win(buf, true, {
+    relative = 'editor',
+    width    = width,
+    height   = height,
+    col      = math.floor((vim.o.columns - width) / 2),
+    row      = math.floor((vim.o.lines - height) / 2),
+    style    = 'minimal',
+    border   = 'rounded',
+  })
+  vim.fn.termopen('lazygit', {
+    on_exit = function() vim.api.nvim_buf_delete(buf, { force = true }) end,
+  })
+  vim.cmd('startinsert')
+end, { desc = 'Open lazygit' })
+
 -- Highlight yanks
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
